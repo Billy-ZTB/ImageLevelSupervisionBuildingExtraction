cc5ec1971b059aea84ba2a0545db9fff16594f5b
Billy-ZTB <billyzhangtianbo@163.com>
2025-09-17 22:59:55 +0800

    更新数据集路径，切换至Potsdam数据集并调整相关加载逻辑

--- dataset_prep/write_numpy.py
+++ dataset_prep/write_numpy.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# File  : write_numpy.py
-# Author: 郑道远
-# Date  : 2021/4/28
 import os
 
 import cv2
@@ -10,11 +5,7 @@ import numpy as np
 
 
 def count_building(label, txt_path):
-    # f = open(txt_path, "w+")
-    # 1.读取标签路径下的掩膜文件
     for _, _, filenames in os.walk(label):
-
-        # 2.循环遍历并统计每个png文件中属于mask = 1的数量
         count1 = count2 = 0
         e = dict()
         for i in range(0, len(filenames)):
@@ -24,22 +15,15 @@ def count_building(label, txt_path):
             assert img.shape[0] == 256 and img.shape[1] == 256,'图片尺寸不对，请检查'
             No_building = np.sum(img == 0)  # 统计背景像素
             building = np.sum(img != 0)  # 统计建筑物像素
-            # 如果当前的 building > Nobuilding，则将该图放置1文件夹中
             if building >= 256 * 256 / 4:
-                # shutil.copyfile(png, os.path.join(save_label, filenames[i]))
-                # f.writelines(filenames[i].split(".")[0] + " 1\n")
                 e[filenames[i].split('.')[0]] = [1,]
                 count1 += 1
-            # 否则，则将该图放置0文件夹中
             elif building == 0:
-                # shutil.copyfile(png, os.path.join(save_label, filenames[i]))
-                # f.writelines(filenames[i].split(".")[0] + " 0\n")
                 e[filenames[i].split('.')[0]] = [0,]
                 count2 += 1
-        # f.close()
         np.save(txt_path, e)
         print(count1, count2)
 
-label = 'C:\ZTB\Dataset\VOC_vaihingen\SegmentationClassAug'
-txt_path ='C:\ZTB\Dataset\VOC_vaihingen/cls_labels.npy'
+label = 'C:\ZTB\Dataset\VOC_potsdam\SegmentationClassAug'
+txt_path ='C:\ZTB\Dataset\VOC_potsdam/cls_labels.npy'
 count_building(label, txt_path)
